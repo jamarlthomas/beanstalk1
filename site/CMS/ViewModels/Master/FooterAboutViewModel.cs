dc7dbfb54565aa78a394e5aa1c2f666e3cfd4aa5
Ivan Rubanov <ivanrubanov@coherentsolutions.com>
2015-11-04 17:53:27 +0300

    add models, providers and viewmodels for footer on master page

---
+++ site/CMS/ViewModels/Master/FooterAboutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMS.Mvc.ViewModels.Master
+{
+    public class FooterAboutViewModel
+    {
+        public string Title { get; set; }
+        public string Reference { get; set; }
+    }
+}
---
+++ site/CMS/ViewModels/Master/FooterNewsEventsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMS.Mvc.ViewModels.Master
+{
+    public class FooterNewsEventsViewModel
+    {
+        public string Title { get; set; }
+        public string Reference { get; set; }
+    }
+}
