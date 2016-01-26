a7483a2c62c5b11da9f24b991a5eef6b23fb05d5
Ivan Rubanau <IvanRubanov@coherentsolutions.com>
2016-01-26 18:05:41 +0300

    fix indents

--- site/CMS/Controllers/Afton/DocumentController.cs
+++ site/CMS/Controllers/Afton/DocumentController.cs
@@ -9,41 +9,41 @@ using CMS.Mvc.ViewModels.Shared;
 
 namespace CMS.Mvc.Controllers.Afton
 {
-	public class DocumentController : BaseController
-	{
-		private readonly IDocumentProvider _documentProvider;
-		private readonly IInsightsResourcesProvider _insightsAndResourcesPageProvider;
+    public class DocumentController : BaseController
+    {
+        private readonly IDocumentProvider _documentProvider;
+        private readonly IInsightsResourcesProvider _insightsAndResourcesPageProvider;
 
-		public DocumentController()
-		{
-			_insightsAndResourcesPageProvider = new InsightsResourcesProvider();
-			_documentProvider = new DocumentProvider();
-		}
+        public DocumentController()
+        {
+            _insightsAndResourcesPageProvider = new InsightsResourcesProvider();
+            _documentProvider = new DocumentProvider();
+        }
 
-		public DocumentController(IInsightsResourcesProvider insightsAndResourcesPageProvider,
-			IDocumentProvider documentProvider)
-		{
-			_insightsAndResourcesPageProvider = insightsAndResourcesPageProvider;
-			_documentProvider = documentProvider;
-		}
+        public DocumentController(IInsightsResourcesProvider insightsAndResourcesPageProvider,
+            IDocumentProvider documentProvider)
+        {
+            _insightsAndResourcesPageProvider = insightsAndResourcesPageProvider;
+            _documentProvider = documentProvider;
+        }
 
-		public ActionResult Index(string name)
-		{
-			var document = _documentProvider.GetDocument(name);
-			if (document == null) return null;
+        public ActionResult Index(string name)
+        {
+            var document = _documentProvider.GetDocument(name);
+            if (document == null) return null;
 
-			var documentViewModel = MapData<Document, DocumentViewModel>(document);
+            var documentViewModel = MapData<Document, DocumentViewModel>(document);
 
-			if (documentViewModel.DocumentPublishFrom == default(DateTime))
-			{
-				documentViewModel.DocumentPublishFrom = (DateTime) document.GetValue("DocumentCreatedWhen");
-			}
+            if (documentViewModel.DocumentPublishFrom == default(DateTime))
+            {
+                documentViewModel.DocumentPublishFrom = (DateTime)document.GetValue("DocumentCreatedWhen");
+            }
 
-			return View("~/Views/Afton/Document/Index.cshtml", new DocumentPageViewModel()
-			{
-				Document = documentViewModel,
-				MenuItemTitle = _insightsAndResourcesPageProvider.GetInsightsResources().First().Title
-			});
-		}
-	}
+            return View("~/Views/Afton/Document/Index.cshtml", new DocumentPageViewModel()
+            {
+                Document = documentViewModel,
+                MenuItemTitle = _insightsAndResourcesPageProvider.GetInsightsResources().First().Title
+            });
+        }
+    }
 }
--- site/CMS/ViewModels/Document/DocumentPageViewModel.cs
+++ site/CMS/ViewModels/Document/DocumentPageViewModel.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 namespace CMS.Mvc.ViewModels.Document
 {
- 	public class DocumentPageViewModel
-	{
-		public DocumentViewModel Document { get; set; }
-		public string MenuItemTitle { get; set; }
-	}
+    public class DocumentPageViewModel
+    {
+        public DocumentViewModel Document { get; set; }
+        public string MenuItemTitle { get; set; }
+    }
 }
