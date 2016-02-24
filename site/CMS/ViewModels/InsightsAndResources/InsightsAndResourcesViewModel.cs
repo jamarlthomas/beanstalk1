02b025bfa75f932f41b9b70f4667af37bf2db0ad
Ivan Rubanau <IvanRubanov@coherentsolutions.com>
2016-02-24 15:54:33 +0300

    integrate insights & resources

--- site/CMS/ViewModels/InsightsAndResources/InsightsAndResourcesViewModel.cs
+++ site/CMS/ViewModels/InsightsAndResources/InsightsAndResourcesViewModel.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 
 namespace CMS.Mvc.ViewModels.InsightsAndResources
 {
-	public class InsightsAndResourcesViewModel
-	{
-		public string Title { get; set; }
-		public string Description { get; set; }
-		public List<InsightsListingItemViewModel> InsightsListing { get; set; }
-		public List<TileViewModel> Tiles { get; set; }
-	}
+    public class InsightsAndResourcesViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string HeroImage { get; set; }
+        public string Headline { get; set; }
+        public string Subheadline { get; set; }
+        public List<InsightsListingItemViewModel> InsightsListing { get; set; }
+        public List<TileViewModel> Tiles { get; set; }
+    }
 }
