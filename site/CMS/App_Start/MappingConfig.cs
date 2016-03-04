3b23df3c0e32d3803ba8a24d814aead0c24025d3
Ivan Rubanau <IvanRubanov@coherentsolutions.com>
2016-03-04 14:21:05 +0300

    add mapping from any CMS class to tile view model

--- site/CMS/App_Start/MappingConfig.cs
+++ site/CMS/App_Start/MappingConfig.cs
@@ -1,16 +1,23 @@
-﻿using CMS.DocumentEngine.Types;
-using CMS.Mvc.ViewModels.Shared;
+﻿using CMS.Mvc.ViewModels.Shared;
+using System;
+using System.Linq;
+using System.Reflection;
 
 namespace CMS.Mvc.App_Start
 {
-	public static class MappingConfig
+    public static class MappingConfig
     {
         public static void RegisterMappings()
         {
-			AutoMapper.Mapper.CreateMap<Document, TileViewModel>();
-			AutoMapper.Mapper.CreateMap<CustomNews, TileViewModel>();
-			AutoMapper.Mapper.CreateMap<Event, TileViewModel>();
-			AutoMapper.Mapper.CreateMap<Solution, TileViewModel>();
+            AnyCMSModelToTileViewModelMapping();
+        }
+
+        private static void AnyCMSModelToTileViewModelMapping()
+        {
+            var tileViewModelType = new TileViewModel().GetType();
+            var cmsTypes = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => String.Equals(t.Namespace, "CMS.DocumentEngine.Types", StringComparison.Ordinal))
+                .Select(type => AutoMapper.Mapper.CreateMap(type, tileViewModelType)).ToList();
         }
     }
 }
