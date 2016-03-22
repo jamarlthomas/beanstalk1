1fac7511e5a06256286cd06119089c13ebb5ad84
Ivan Rubanau <IvanRubanov@coherentsolutions.com>
2016-03-22 11:27:01 +0300

    add cms part for contact page

---
+++ site/CMS/Providers/SalesOfficeProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using CMS.DocumentEngine;
+using CMS.DocumentEngine.Types;
+using CMS.Mvc.Helpers;
+using CMS.Mvc.Interfaces;
+
+namespace CMS.Mvc.Providers
+{
+    public class SalesOfficeProvider : ISalesOfficeProvider
+    {
+        public List<SalesOffice> GetSalesOffices(string parentName)
+        {
+            return ContentHelper.GetDocChildrenByName<SalesOffice>(SalesOffice.CLASS_NAME, parentName);
+        }
+
+        public SalesOffice GetPrimarySalesOffice(string parentName)
+        {
+            return GetSalesOffices(parentName).First();
+        }
+    }
+}
---
+++ site/CMS/ViewModels/Contact/ContactPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CMS.Mvc.ViewModels.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMS.Mvc.ViewModels.Contact
+{
+    public class ContactPageViewModel
+    {
+        public string Title { get; set; }
+        public string FormIntroducionText { get; set; }
+        public string FirstNamePlaceholder { get; set; }
+        public string LastNamePlaceholder { get; set; }
+        public string CompanyNamePlaceholder { get; set; }
+        public string EmailPlaceholder { get; set; }
+        public string PhonePlaceholder { get; set; }
+        public string SelectCountryPlaceholder { get; set; }
+        public List<ContactCountryViewModel> Countries { get; set; }
+        public string MessagePlaceholder { get; set; }
+        public string NewsletterTitle { get; set; }
+        public string NewsletterCheckboxLabel { get; set; }
+        public string SubmitLabel { get; set; }
+        public string PromptLabel { get; set; }
+
+        public List<ContactRegionViewModel> Regions { get; set; }
+        public EmergencyResponseViewModel EmergencyResponse { get; set; }
+        public BreadCrumbViewModel BreadCrumb { get; set; }
+    }
+}
