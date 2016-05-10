9f7d6c4edb31742155da5426df951d25a4f80533
Rohit Keshwani <rohit.keshwani@padillacrt.com>
2016-05-09 23:46:24 -0500

    Added Extended models and javascript for stay informed

---
+++ site/CMS/Models/ExtendedModels/FAQPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using CMS.Mvc.Helpers;
+using CMS.Mvc.Interfaces;
+
+namespace CMS.DocumentEngine.Types
+{
+    public partial class FAQPage : IRoutedModel
+    {
+        public string DocumentRoutePath
+        {
+            get
+            {
+                var rt = RouteHelper.GetRoute("FAQ");
+                return (rt != null) ? rt.Route : "/FAQPage/Index";
+            }
+        }
+    }
+}
