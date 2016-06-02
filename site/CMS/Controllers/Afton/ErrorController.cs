eb438654aa56eef56f2718527420f3a37aa062fc
Creative.Temp2 <Creative.Temp2@padillacrt.com>
2016-06-02 16:33:01 -0500

    404 Error page

---
+++ site/CMS/Controllers/Afton/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Mvc.Controllers.Afton
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("~/Views/Afton/Shared/Error.cshtml");
+        }
+	}
+}
