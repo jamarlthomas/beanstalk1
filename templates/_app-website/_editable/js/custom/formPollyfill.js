c14e93900f606e7efc8a6273a09f601814b8dddd
John Flynn <john.flynn@padillacrt.com>
2016-06-02 16:57:17 -0500

    Afton-942: Made a CSS change for the contact box to address multiple lines.  Updated polyfill file to help validate the email address and the phone number

--- templates/_app-website/_editable/js/custom/formPollyfill.js
+++ templates/_app-website/_editable/js/custom/formPollyfill.js
@@ -40,5 +40,54 @@ $( document ).ready(function() {
 
     }
     
+    //Other Validations
+    $(document).on("submit", function(e) {
+        
+        var validateFields = [];
+        
+        //validate email address
+        $(this)
+            .find("input")
+            .filter("[email]")
+            .each(function() {
+                
+                var getEmailAddress = $(this).val();
+                var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+                var emailResult = pattern.test(getEmailAddress);
+                
+                //if email address failed
+                if(!emailResult){ 
+                    e.preventDefault();
+                    $(this).css({ "border-color":"red" });
+                    validateFields.push( "Email Address" )  
+                }
+                
+            });
+        
+        //validate phone number
+        $(this)
+            .find("input")
+            .filter("[phone]")
+            .each(function() {
+                
+                var getPhone = $(this).val();
+                var phoneResult = getPhone.replace(/\D/g, "");
+                
+                console.log(getPhone.replace(/\D/g, ""))
+            
+                //if email address failed
+                if(!phoneResult){ 
+                    e.preventDefault();
+                    $(this).css({ "border-color":"red" });
+                    validateFields.push( "Phone Number" ) 
+                }
+                
+            });
+
+            if(validateFields.length >  0){
+                alert( "The following fields didn't validate: " + validateFields.toString() );
+            }
+    
+    });
     
 });
