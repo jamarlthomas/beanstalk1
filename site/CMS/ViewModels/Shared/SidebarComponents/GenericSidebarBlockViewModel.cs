8b486928a9eb76f35980269e767db430fa2de8a0
Creative.Temp2 <Creative.Temp2@padillacrt.com>
2016-06-08 12:51:08 -0500

    Updated Code for Description Field to override the description message of the sidebar.

--- site/CMS/ViewModels/Shared/SidebarComponents/GenericSidebarBlockViewModel.cs
+++ site/CMS/ViewModels/Shared/SidebarComponents/GenericSidebarBlockViewModel.cs
@@ -8,6 +8,10 @@ namespace CMS.Mvc.ViewModels.Shared.SidebarComponents
         public GenericSidebarBlockViewModel(TreeNode item) : base(item)
         {
             DefaultImage = ((GenericSidebarBlock) item).DefaultImage;
+            if (!string.IsNullOrEmpty(((GenericSidebarBlock)item).Description))
+            {
+                Description = new System.Web.HtmlString(((GenericSidebarBlock)item).Description);
+            }
         }
     }
 }
