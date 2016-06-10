8c50fb732091fe2f1f46044e708a77e03a79f93e
Rohit Keshwani <rohit.keshwani@padillacrt.com>
2016-06-10 14:00:08 -0500

    Resolved Validation Issue

--- site/CMS/Scripts/custom/stayInformed.js
+++ site/CMS/Scripts/custom/stayInformed.js
@@ -1,9 +1,17 @@
 ﻿$(document).ready(function () {
-    $(".sidebarBoxC input.submitBtn").submit(submitEmail);
+    $(".sidebarBoxC input.submitBtn").click(submitEmail);
     function submitEmail() {
         var email = $(".sidebarBoxC input[name=email]").val();
-        $.get('/SidebarPage/SubmitEmail', { email: email }, function () {
-            alert("Thank you for your submission");
-        });
+        var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        var emailResult = pattern.test(email);
+
+        if (emailResult) {
+            $.get('/SidebarPage/SubmitEmail', { email: email }, function () {
+                alert("Thank you for your submission");
+            });
+        }
+        /*else {
+            alert("The following fields are required: Email Address");
+        }*/
     }
 });
