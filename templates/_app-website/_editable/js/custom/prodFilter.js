caabb29b821387d83d237917a4565a57b439ae17
Rohit Keshwani <rohit.keshwani@padillacrt.com>
2016-12-27 16:37:54 -0600

    Added Advanced Tracking to Selection Filter Added Hotjar Cookie fix

--- templates/_app-website/_editable/js/custom/prodFilter.js
+++ templates/_app-website/_editable/js/custom/prodFilter.js
@@ -455,12 +455,13 @@ app.controller('prodFilterCntl', ["$scope", "$location", "$http", "$timeout", fu
         
         
         //send GA Event
-        
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        //No need to load up GA again as ga is global object -RHK 12/27/2016
+        /*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
           })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
+        */
+        
         ga('create', 'UA-10080511-2', 'auto');
         ga('send', 'pageview');
           
@@ -471,8 +472,8 @@ app.controller('prodFilterCntl', ["$scope", "$location", "$http", "$timeout", fu
         gaEventLabel = gaEventLabel + " - SOLUTIONS = " + $scope.solutionArrayText
         //gaEventLabel = gaEventLabel + " - SORT = " + $scope.sortItems
         //gaEventLabel = gaEventLabel + " - PAGE = " + $scope.pageNumber
-        //gaEventLabel = gaEventLabel + " - SEARCH = " + $scope.searchFilter
-        //console.log(gaEventLabel)
+        gaEventLabel = gaEventLabel + " - SEARCH = " + $scope.searchFilter
+        console.log(gaEventLabel)
         
         ga('send', 'event', {
             eventCategory: 'product filter',
