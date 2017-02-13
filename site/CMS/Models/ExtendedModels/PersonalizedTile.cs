7f527926b5b7adf1ac3acff2396433f658d4da6f
Rohit Keshwani <rohit.keshwani@padillacrt.com>
2017-02-13 16:10:18 -0600

    Made English Default tile language but tried to pulling currently active culture first.

--- site/CMS/Models/ExtendedModels/PersonalizedTile.cs
+++ site/CMS/Models/ExtendedModels/PersonalizedTile.cs
@@ -7,12 +7,22 @@ namespace CMS.DocumentEngine.Types
         public string Reference { get; set; }
         internal void Load(TreeNode item)
         {
-            Item = item;
-            HomeImage = (string)item.GetValue("HomeImage");
-            TileTitle = (string)item.GetValue("TileTitle");
-            Title = (string)item.GetValue("Title");
-            Description = (string)item.GetValue("Description");
-            Reference = ((item as IRoutedModel) != null) ? ((IRoutedModel) item).DocumentRoutePath : item.DocumentNamePath;
+            var test = DocumentHelper.GetDocument( item.NodeID,Localization.LocalizationContext.PreferredCultureCode ,item.TreeProvider );
+            //Item = item;
+            if ( test != null && test.IsPublished==true)
+            {
+                Item = test;
+            }
+            else
+            {
+                Item = DocumentHelper.GetDocument( item.NodeID, "en-US", item.TreeProvider ); ;
+            }
+            HomeImage = (string)Item.GetValue("HomeImage");
+            TileTitle = (string)Item.GetValue("TileTitle");
+            Title = (string)Item.GetValue("Title");
+            Description = (string)Item.GetValue("Description");
+            Reference = ((Item as IRoutedModel) != null) ? ((IRoutedModel) Item).DocumentRoutePath : Item.DocumentNamePath;
+
 
         }
 
